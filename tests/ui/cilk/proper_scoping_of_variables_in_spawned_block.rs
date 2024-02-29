da93a204b35686952a4a271a096a33d9f81c5132
jhilton <jhilton@mit.edu>
2024-02-29 11:29:00 -0500

    Add UI test for scoping of variables defined in spawned block
    Adds a test that variables defined in a spawned block are not usable in the
    continuation because of scoping rules (rather than undefined variables).

---
+++ tests/ui/cilk/proper_scoping_of_variables_in_spawned_block.rs
@@ -0,0 +1,6 @@
+// Tests that when a variable is declared in a spawned block, it's not usable outside that spawned block.
+fn main() {
+    let _ = cilk_spawn { let y = 5; y };
+    println!("y={}", y);
+//~^ ERROR cannot find value `y` in this scope [E0425]
+}
