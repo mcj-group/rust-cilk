1ed023529e6e09ead695776ff36e786a8c6c54ec
jhilton <jhilton@mit.edu>
2024-03-02 14:27:44 -0500

    borrows_dropped_at_sync -> borrows_dropped_after_sync, add useful comment

---
+++ tests/ui/cilk/borrows_dropped_after_sync.rs
@@ -0,0 +1,14 @@
+// Tests that borrows are not live after the cilk_sync point.
+// build-pass
+
+// Note that this test can pass if the borrow is dropped at the end of the block being spawned
+// rather than at the sync. There's another test that borrows are live before a sync.
+fn main() {
+    let mut s = String::from("hello");
+    cilk_spawn { 
+        println!("{}", &s);
+    };
+    cilk_sync;
+    s.push_str(" world");
+    println!("{}", s);
+}
