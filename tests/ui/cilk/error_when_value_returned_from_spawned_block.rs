0f48ef43f7d2d84c78995f309e6a9d68dfb802d1
jhilton <jhilton@mit.edu>
2024-04-05 02:09:58 -0400

    Add test that an error occurs when we return from a spawned block

---
+++ tests/ui/cilk/error_when_value_returned_from_spawned_block.rs
@@ -0,0 +1,10 @@
+// Tests that compilation fails when a value is returned from a spawned block before
+// a sync runs.
+
+fn main() {
+    let x = { 
+        let y = cilk_spawn { 1 };
+        y
+//~^ ERROR: used binding `y` is possibly-uninitialized [E0381]
+    };
+}
