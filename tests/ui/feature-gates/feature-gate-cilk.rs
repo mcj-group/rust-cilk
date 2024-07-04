1506353b71fde0e99f7569da2b35c5ee92e57dbe
jhilton <jhilton@mit.edu>
2024-07-04 17:32:36 -0400

    Add tests for feature gate and simple loop semantics

--- tests/ui/feature-gates/feature-gate-cilk.rs
+++ tests/ui/feature-gates/feature-gate-cilk.rs
@@ -2,4 +2,5 @@ fn main() {
     cilk_spawn { 5 }; //~ ERROR cilk keywords are experimental [E0658]
     cilk_sync; //~ ERROR cilk keywords are experimental [E0658]
     cilk_scope { 5 }; //~ ERROR cilk keywords are experimental [E0658]
+    cilk_for i in 0..10 {} //~ ERROR cilk keywords are experimental [E0658]
 }
