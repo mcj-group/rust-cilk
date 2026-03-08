5ba8c15925f069d461c8986e3b39924cb2723eea
Parisa Betel Miri <parisa.betel.miri@gmail.com>
2026-03-08 10:24:42 -0400

    Allow labels on cilk_for

--- compiler/rustc_parse/src/parser/expr.rs
+++ compiler/rustc_parse/src/parser/expr.rs
@@ -1715,6 +1715,8 @@ impl<'a> Parser<'a> {
             self.parse_expr_while(label, lo)
         } else if self.eat_keyword(exp!(For)) {
             self.parse_expr_for(label, lo)
+        } else if self.eat_keyword(exp!(CilkFor)) {
+            self.parse_expr_cilk_for(label, lo)
         } else if self.eat_keyword(exp!(Loop)) {
             self.parse_expr_loop(label, lo)
         } else if self.check_noexpect(&token::OpenBrace) || self.token.is_metavar_block() {
