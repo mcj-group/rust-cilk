c47f0e5f71e6098aa0133c232243c3d6f05ca311
Shengwen Chang <shengwen.chang@mail.utoronto.ca>
2026-07-22 21:22:07 +0000

    Added cilk_spawn_break and nested_cilk_for_continue_outer tests.

--- tests/ui/cilk/cilk_spawn_break.rs
+++ tests/ui/cilk/cilk_spawn_break.rs
@@ -1,12 +1,13 @@
 #![feature(cilk)]
 // Tests that breaking from a cilk_spawn is not allowed.
 
-//@ known-bug: unknown
 //@ compile-flags: -C panic=abort
 //@ no-prefer-dynamic
 
 fn main() {
     for _ in 0..5 {
-        cilk_spawn { break; };
+        cilk_spawn { 
+            break; //~ error: cannot use `break` within cilk_spawn
+        };
     }
 }
